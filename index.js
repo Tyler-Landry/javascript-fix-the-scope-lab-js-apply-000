ac513f23c5a74dc73a9e264a9915c565861f1d30
Tyler Landry <tyler.landry1996@outlook.com>
2019-02-25 04:51:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ var funkyFunction = function() {
   return function() {
   return "FUNKY!"
   }
-  { var theFunk = function(); {
+  { var theFunk = function() {
   return function(funkyFunction) {
     }
   }
